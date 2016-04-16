0b8582e4f378e28e162c007409ff0330b4690915
Deepak Grover <deepakgrover333@gmail.com>
2016-04-16 21:22:46 +0530

    add basic components for profile

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var USER_DATA = {
+    name: 'Deepak Grover',
+    username: 'metagrover',
+    image: 'https://avatars1.githubusercontent.com/u/9116042?v=3&s=460'
+};
+
+var ProfilePic = React.createClass({
+    render: function() {
+        return <img src={this.props.imageUrl} style={{height: 100, width: 100}} /> 
+    }
+});
+
+var ProfileLink = React.createClass({
     render: function() {
         return (
-            <div>Hello World</div>
+            <div>
+                <a href={'https://github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
+        );
+    }
+});
+
+var ProfileName = React.createClass({
+    render: function() {
+        return (
+            <div>{this.props.name}</div>
+        );
+    }
+});
+
+var Avatar = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
+            </div>
         );
     }
 });
 
 ReactDOM.render(
-    <HelloWorld />,
+    <Avatar user={USER_DATA} />,
     document.getElementById('app')  
 );
