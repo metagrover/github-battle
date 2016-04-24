ffd4066e26a5e2a43f1441b64b665cd669604a2f
Deepak Grover <deepakgrover333@gmail.com>
2016-04-24 18:51:57 +0530

    add stateless functional component

---
+++ app/components/Prompt.js
@@ -0,0 +1,41 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+var transparentBg = require('../styles').transparentBg;
+
+var Prompt = function(props) {
+    return (
+        <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>       
+            <h1>{props.header}</h1>
+            <br/>
+
+            <div className="col-sm-12">
+                <form onSubmit={props.onSubmitUser}>   
+                    <div class="form-group">
+                        <input 
+                            className="form-control" 
+                            placeholder="Github Username"
+                            onChange={props.onUpdateUser}
+                            value={props.username}
+                            type="text" />
+                    </div>
+                    <br/>
+
+                    <div className="form-group col-sm-4 col-sm-offset-4">
+                        <button className="btn btn-block btn-success" type="submit">
+                            Continue
+                        </button>
+                    </div> 
+                </form>
+            </div>
+        </div>
+    );
+};
+
+Prompt.propTypes = {
+    header: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    onSubmitUser: PropTypes.func.isRequired,
+    onUpdateUser: PropTypes.func.isRequired
+};
+
+module.exports = Prompt;
--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,25 +1,50 @@
 var React = require('react');
-var transparentBg = require('../styles').transparentBg;
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
+    contextTypes: {
+        router: React.PropTypes.object.isRequired
+    },
+
+    getInitialState: function() {
+        return {
+            username: ''
+        }
+    },
+
+    handleUpdateUser: function(e) {
+        this.setState({
+            username: e.target.value
+        })
+    },
+
+    handleSubmitUser: function(e) {
+        e.preventDefault();
+        let username = this.state.username;
+        this.setState({
+            username: ''
+        });
+
+        if (this.props.routeParams.playerOne) {
+            this.context.router.push({
+                pathname: '/battle',
+                query: {
+                    playerOne: this.props.routeParams.playerOne,
+                    playerTwo: username
+                }
+            });
+        } else {
+            this.context.router.push('/playerTwo/' + username);
+        }
+    },
+
     render: function() {
-        console.log(this);
         return (
-            <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-                <h1>{this.props.route.header}</h1><br/>
-                <div className="col-sm-12">
-                    <form>
-                        <div class="form-group">
-                            <input className="form-control" placeholder="Github Username" type="text" />
-                        </div><br/>
-                        <div className="form-group col-sm-4 col-sm-offset-4">
-                            <button className="btn btn-block btn-success" type="submit">
-                                Continue
-                            </button>
-                        </div> 
-                    </form>
-                </div>
-            </div>
+            <Prompt 
+            onSubmitUser={this.handleSubmitUser}
+            onUpdateUser={this.handleUpdateUser}
+            header={this.props.route.header}
+            username={this.state.username} />
         );
     }
 });
